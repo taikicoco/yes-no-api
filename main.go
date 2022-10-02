70f993ed164b1522324527bf67e7ec6f1ab2cef1
taikicoco <kawasakitaiki0308@gmail.com>
2022-10-02 20:36:25 +0900

    add/get_json_data

--- main.go
+++ main.go
@@ -1,58 +1,27 @@
 package main
 
 import (
-	_"fmt"
-	"net/http"
+	"api/handler"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
+var err error
+
 func main() {
-  
   e := echo.New()
 
-  // ミドルウェア
+  // middleware
   e.Use(middleware.Logger())
   e.Use(middleware.Recover())
 
-  // ルート
-  e.GET("/", hello) 
-  e.GET("/get/:id", get)
-  e.GET("/json",getjson)
-  e.POST("getname",getname)
+  if err = handler.Routes(e); err != nil {
+    panic(err)
+  }
 
-  // サーバーをポート番号1323で起動
-  e.Logger.Fatal(e.Start(":1323"))
-  
+  // start server
+  e.Logger.Fatal(e.Start(":1333"))
 }
 
-// ハンドラーを定義
-func hello(c echo.Context) error {
-  return c.String(http.StatusOK, "Hello, World!")
-}
 
-func get(c echo.Context) error {
-  id := c.Param("id")
-  return c.String(http.StatusOK, id)
-}
-
-func getjson(c echo.Context) error {
-  return c.JSON(http.StatusOK, map[string]string{"hello": "world"})
-}
-
-func getname(c echo.Context) error {
-  param := new(HelloParam)
-    if err := c.Bind(&param); err != nil {
-        return err
-    }
-    return c.JSON(http.StatusOK, map[string]interface{}{"hello": param.GreetingTo})
-}
-
-type User struct {
-  Name  string `json:"name"`
-}
-
-type HelloParam struct {
-  GreetingTo string `json:"greetingto"`
-}
